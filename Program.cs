bd9a14af73f4ea7184f0a6cca165ea507ef76c91
MiltonQuirino <miltonquirino.br@gmail.com>
2022-04-16 19:08:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+using System.Text;
+using ApiAuth;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+
+
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,
+//         options => builder.Configuration.Bind("JwtSettings", options));
+    // .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+    //     options => builder.Configuration.Bind("CookieSettings", options));
+
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+//         options.RequireHttpsMetadata = false;
+//         options.SaveToken = true;
+//         options.TokenValidationParameters =
+//             new TokenValidationParameters
+//             {
+//                 ValidateAudience = false,
+//                 ValidateIssuer = false,
+//                 ValidateActor = false,
+//                 ValidateLifetime = true,
+//                 IssuerSigningKey = new SymmetricSecurityKey(key),
+//             };
+//     });
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+using ApiAuth.Models;
+
+namespace ApiAuth.Repositories;
+
+public class UserRepository
+{
+    public static User Get(string username, string password)
+    {
+        var users = new List<User>();
+        users.Add(new User { Id = 1, Username = "batman", Password = "batman", Role = "manager" });
+        users.Add(new User { Id = 2, Username = "robin", Password = "robin", Role = "employee" });
+        return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+    }
+}
